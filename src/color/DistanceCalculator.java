c72108d07f92626247307c048555c66f40861700
demourak <kdemoura121@gmail.com>
2016-03-29 10:45:04 -0400

    created DistanceCalculator and Flight partial skeletons

---
+++ src/color/DistanceCalculator.java
@@ -0,0 +1,18 @@
+package color;
+
+import flightData.Flight;
+
+public class DistanceCalculator {
+    public static void main(String[] args) {
+        System.out.println("Hello.");
+    }
+
+    private float distance(Flight f1, Flight f2) {
+
+        return 0;
+    }
+
+    private float angle(Flight f1, Flight f2) {
+        return 0;
+    }
+}
---
+++ src/flightData/Flight.java
@@ -0,0 +1,7 @@
+package flightData;
+
+/**
+ * Created by Kevin on 3/29/2016.
+ */
+public class Flight {
+}
