1f6c1106df2e63c05c1106da887207090e5c41a0
Jordan <jordannstrong@gmail.com>
2016-04-10 12:02:12 -0400

    Basic function to calculate the distance between two sets of coordinates

--- src/color/DistanceCalculator.java
+++ src/color/DistanceCalculator.java
@@ -7,12 +7,12 @@ public class DistanceCalculator {
         System.out.println("Hello.");
     }
 
-    private float distance(Flight f1, Flight f2) {
-
-        return 0;
+    private double distance(Flight f1, Flight f2) {
+        double dLat = Math.toRadians(f2.getLatitude()-f1.getLatitude());
+        double dLon = Math.toRadians(f2.getLongitude()-f1.getLongitude());
+        double distance = (dLon * dLon) + (dLat *dLat);
+        return Math.sqrt(distance);
     }
 
-    private float angle(Flight f1, Flight f2) {
-        return 0;
-    }
+
 }
