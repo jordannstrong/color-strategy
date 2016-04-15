fd9d62681c03ce7007e9f90edb2900875c37006c
Jordan <jordannstrong@gmail.com>
2016-04-15 12:02:47 -0400

    Moving database files from / to /src/database

--- src/color/DistanceCalculator.java
+++ src/color/DistanceCalculator.java
@@ -17,12 +17,12 @@ public class DistanceCalculator {
         System.out.println("Hello.");
     }
 
-    private double distance(Flight f1, Flight f2) {
+    /*private double distance(Flight f1, Flight f2) {
         double dLat = Math.abs(f2.getLatitude()-f1.getLatitude());
         double dLon = Math.abs(f2.getLongitude()-f1.getLongitude());
         double distance = (dLon * dLon) + (dLat * dLat);
         return Math.sqrt(distance);
-    }
+    }*/
 
 
     public List<Flight> getSortedFlights()
