2659d5b332be693bd565a8b57c551384975a38e8
sbialt <sbialt@gmail.com>
2016-04-17 13:13:36 -0400

    Added files via upload
    Mock files for screenshot purposes

---
+++ src/mock/main.java
@@ -0,0 +1,10 @@
+package color;
+
+class main {
+    public static void main(String[] args)
+     {
+        //Uncomment whichever one you want to take a screenshot of
+        ParameterChooserMock mockParam = new ParameterChooserMock();
+        ColumnChooserMock mockColumn = new ColumnChooserMock();
+    }
+}
